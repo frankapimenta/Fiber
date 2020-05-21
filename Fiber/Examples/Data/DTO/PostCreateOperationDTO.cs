c4e49486b884f1f00997872e7f68ba2f0cdf0257
Frank Pimenta <frank.pimenta@protonmail.com>
2020-05-21 22:57:39 +0200

    . Rename example model to dto.

---
+++ Fiber/Examples/Data/DTO/PostCreateOperationDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fiber.Examples.Data
+{
+    public class PostCreateOperationDTO
+    {
+        [Required]
+        public string Title { get; set; }
+        public string Body { get; set; }
+        
+    }
+}
--- Fiber/Examples/Data/Model/PostCreateOperationModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Fiber.Examples.Data
-{
-    class PostCreateOperationModel
-    {
-        public string Title { get; set; }
-        public string Body { get; set; }
-    }
-}
