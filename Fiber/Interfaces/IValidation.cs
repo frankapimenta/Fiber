0a550afbdf8386a9685305bb944e38bbbca8651a
Frank Pimenta <frank.pimenta@protonmail.com>
2020-05-21 22:59:53 +0200

    + Add Validation adapters.

---
+++ Fiber/Interfaces/IValidation.cs
@@ -0,0 +1,11 @@
+﻿namespace Fiber.Interfaces
+{
+    public interface IValidation<T>
+    {
+        public IValidation<T> Model { get; }
+        public virtual bool Valid()
+        {
+            return Model.Valid();
+        }
+    }
+}
---
+++ Fiber/Validations/Adapters/ValidationAdapter.cs
@@ -0,0 +1,20 @@
+﻿using Fiber.Interfaces;
+
+namespace Fiber.Validations.Adapters
+{
+    public class ValidationAdapter<T> : IValidation<T>
+    {
+        private readonly IValidation<T> model;
+        public ValidationAdapter(IValidation<T> model)
+        {
+            this.model = model;
+        }
+
+        public IValidation<T> Model => this.model;
+
+        public virtual bool Valid()
+        {
+            return this.model.Valid();
+        }
+    }
+}
