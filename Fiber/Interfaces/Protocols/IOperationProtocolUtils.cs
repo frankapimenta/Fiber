7fcfe64ffaa91e59c64887308f303f9d2a838449
Frank Pimenta <frank.pimenta@protonmail.com>
2020-05-24 14:13:05 +0200

    + Add interface IOperationProtocolUtils.

---
+++ Fiber/Interfaces/Protocols/IOperationProtocolUtils.cs
@@ -0,0 +1,12 @@
+﻿using Fiber.Errors;
+using Fiber.Interfaces.Operations;
+
+namespace Fiber.Interfaces.Protocols
+{
+    public interface IOperationProtocolUtils<T,U,V> where T : class, new() where U : class, new() where V : class, new()
+    {
+        public bool Validate<ValidationAdapterClass>(IOperationAction<T, U, V> operationAction);
+        public IOperationAction<T, U, V> CreateInvalidResponse(IOperationAction<T, U, V> action);
+        public IOperationAction<T, U, V> AddResponseToAction(IOperationAction<T, U, V> operationAction, IInvalidResponse<IError> invalidResponse);
+    }
+}
